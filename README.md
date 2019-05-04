b8e0440b0c2f3f37a16aa069aacd9bc9e6d7aa88
BadrAlnassar <badrabdulaziz.24@gmail.com>
2019-05-04 08:25:42 +0300

    after change

--- README.md
+++ README.md
@@ -1,20 +1,10 @@
-# Classic Arcade Game Clone Project
+# Memory Game Project
+This game is a required project in Udacity's Front-End Nanodegree Program.
 
-## Table of Contents
+## Project Idea
 
-- [Instructions](#instructions)
-- [Contributing](#contributing)
+The project idea is a classic arcade game where your character should get to the river without colliding with the enemy and the purpose of this project is to demonstrate the knowledge of Javascript Objects
 
-## Instructions
-
-Use this [rubric](https://review.udacity.com/#!/rubrics/15/view) for self-checking your submission.
-
-Make sure the functions you write are **object-oriented** - either class functions (like `Player` and `Enemy`) or class prototype functions such as `Enemy.prototype.checkCollisions`. Also make sure that the keyword `this` is used appropriately within your class and class prototype functions to refer to the object the function is called upon.
-
-Your **README.md** file should be updated with instructions on both how to 1. Run and 2. Play your arcade game.
-
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
-
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+### Instructions to run the game:
+1. Open classicArcadeGame folder.
+2. run index.html.
--- js/app.js
+++ js/app.js
@@ -1,34 +1,130 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// var Enemy = function() {
+//     // Variables applied to each of our instances go here,
+//     // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+//     // The image/sprite for our enemies, this uses
+//     // a helper we've provided to easily load images
+//     this.sprite = 'images/enemy-bug.png';
+// };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+// Enemy.prototype.update = function(dt) {
+//     // You should multiply any movement by the dt parameter
+//     // which will ensure the game runs at the same speed for
+//     // all computers.
+// };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+// Enemy.prototype.render = function() {
+//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// };
+
+class Enemy{
+
+    constructor(x, y, speed){
+        this.sprite = 'images/enemy-bug.png'
+        this.x = x
+        this.y = y
+        this.speed = speed
+    }
+
+    update(dt){  
+        this.x += (dt * this.speed)
+
+        if(this.x >= 400){
+            this.x = 0
+        }
+        allEnemies.forEach(el => { 
+            let X = Math.round( el.x ) 
+            let Y = Math.round( el.y )
+            if(this.x == X && this.y == Y){ 
+                player.x = 200
+                player.y = 400
+                
+            }
+
+        })
+    }
+
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+class Player{
+
+    constructor(x, y){
+        this.sprite = 'images/char-boy.png'
+        this.x = x
+        this.y = y
+    }
+
+    update(dt){
+        
+        allEnemies.forEach(el => { 
+            let X = Math.round( el.x ) 
+            let Y = Math.round( el.y )
+            if(this.x == X && this.y == Y){ 
+                window.location.reload();
+                
+            }
+
+        })
+    }
+
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(keyCode){
+        
+        if(keyCode == 'up' && this.y > 0){
+            this.y = this.y - 100
+        }
+        if(keyCode == 'right' && this.x < 400){
+            this.x = this.x + 100
+        }
+        if(keyCode == 'down' && this.y < 400){
+            this.y = this.y + 100
+        }
+        
+        if(keyCode == 'left' && this.x > 0){
+            this.x = this.x - 100
+        }
+
+
+        if(this.y == 0){
+            this.x = 200
+            this.y = 400
+            swal({
+                title: "Good job, You Win!!",
+                
+                icon: "success",
+                button: "play agin",
+              });
+        }
+        
+        
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const player = new Player(200,400)
+const E = new Enemy(0,100,100)
+const E1 = new Enemy(0,200,110)
+
+var allEnemies = []
+var allPlayers = []
+allEnemies.push(E)
+allEnemies.push(E1)
 
 
 
